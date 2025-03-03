bad5f5d48d42351c12f4bbeb724a52967dea2c0b
keinagic <103723895+keinagic@users.noreply.github.com>
2025-03-03 14:59:04 +0800

    Database creation foolproofing

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-if __name__ == "main.py":
-    print("Hello World")
-    pass
+import kots_interface.main_window as main_window
+import os
+from data import database
+
+
+class FoolProofing:
+    @staticmethod
+    def create_tables():
+        conn = database.DBConnection.create_connection()
+        if not os.path.exists(database.DB_PATH):
+            try:
+                database.DBConnection.create_tables(conn)
+                print("Tables created successfully")
+            except Exception as e:
+                print(f"Error creating tables: {e}")
+            finally:
+                conn.close()
+        else:
+            print("Database already exists")
+
+
+if __name__ == "__main__":
+    FoolProofing.create_tables()
+    main_window.main()
